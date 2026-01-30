4d556f6b6d8cb17b1c9b18305427658e3c0bf3ac
Thomaz <thomazx4v13r@gmail.com>
2026-01-30 18:58:30 -0300

    Adiciona Scripts do Grid

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int _width, _height;
+
+    [SerializeField] private Tile _tilePrefab;
+
+    [SerializeField] private Transform _cameraTransform;
+
+    private Dictionary<Vector2, Tile> _tiles = new Dictionary<Vector2, Tile>();
+
+    void Start()
+    {
+        startGrid();
+    }
+
+
+    void startGrid()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector2(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+
+       
+                _tiles[new Vector2(x,y)] = spawnedTile;
+
+            }
+        }
+
+        _cameraTransform.position = new Vector3((float) _width/2 - 0.5f, (float) _height/2 - 0.5f, -10);
+
+
+    }
+
+    public Tile getTilePosition(Vector2 pos)
+    {
+        if (_tiles.TryGetValue(pos, out var tile))
+        {
+            return tile;
+        }
+        return null;
+    }
+
+
+
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Color _color1, _color2;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private GameObject _highLight;
+
+
+    public void setColot(bool isDiferent)
+    {
+        _spriteRenderer.color = isDiferent ? _color1 : _color2;
+    }
+
+    private void OnMouseEnter()
+    {
+        _highLight.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        _highLight.SetActive(false);
+    }
+
+
+}
